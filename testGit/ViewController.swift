ff0f6a57aabb0aacc805e9dde416bfbf2a1219e9
Tiziano <tiziano@Tiziano-MacBook-Pro.local>
2019-04-07 01:03:28 +0100

    Initial Commit

---
+++ testGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Tiziano on 07/04/2019.
+//  Copyright © 2019 Tiziano Bissola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
